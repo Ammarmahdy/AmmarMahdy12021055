150cbbcd6d7c3598f7a93ef53d142214aa36259e
Ammarmahdy <149155111+Ammarmahdy@users.noreply.github.com>
2023-10-31 03:24:35 +0700

    tugas matlab matrik 1

---
+++ AmmarMahdy112021055tugasmatlab.m
@@ -0,0 +1,99 @@
+%Ammar mahdy
+%112021055
+
+%Matrik 
+A=[1 2 3; 2 1 1; 3 2 1]
+%4.3
+B=[4 4 5;6 1 2;3 5 5]
+
+%Determinan
+Aa= det(A)
+Ab= det(B)
+
+%Ukuran
+Ba= size(A)
+Bb= size(B)
+
+%Trace
+Ca= trace(A)
+Cb= trace(B)
+
+%Norm
+Da= norm(A)
+Db= norm(B)
+
+%E.
+ce= A+B
+
+%F. 
+cf= A-B
+
+%G.
+cg= A*B
+
+%H.
+ch= A.*B
+
+%I.
+ci= A^2
+
+%J. 
+cj= A.^2
+
+%K.
+ak= A'
+bk= B'
+
+%L. 
+cl= A./B
+
+%M.
+cm= A.\B 
+
+%N. 
+cn= A/B
+
+%O.
+ao= inv(A)
+bo= inv(B)
+
+%P.
+cp= null(A)
+
+%Q.
+cq= orth(A)
+
+%R.
+cr= rref(A)
+
+%S.
+as= eig(A)
+bs= eig(B)
+
+%T. 
+at= svd(A)
+bt= svd(B)
+
+%U.
+au= triu(A)
+bu= triu(B)
+
+%V.
+av= tril(A)
+bv= tril(B)
+
+%W.
+aw= max (max(A))
+bw= max (max(B))
+
+%X.
+ax= min (min(A))
+bx= min (min(B))
+
+%Y.
+ay= length(A)
+by= length(B)
+
+%Z.
+az= diag(A)
+bz= diag(B)
